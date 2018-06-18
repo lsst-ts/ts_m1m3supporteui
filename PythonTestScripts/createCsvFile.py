df12c21d6e6282bfeee5beed97d4f0a710c57e1e
Andy Clements <aclements@lsst.org>
2018-06-18 15:46:41 -0700

    adding last commands

--- PythonTestScripts/createCsvFile.py
+++ PythonTestScripts/createCsvFile.py
@@ -2,10 +2,12 @@
 
 import csv
 import random
+import numpy
 
-out = csv.writer(open("MirrorProfileForces.csv","w"), delimiter='\t',quoting=csv.QUOTE_NONE)
+out = csv.writer(open("MirrorProfileForces.csv","w"), delimiter='\t', quoting=csv.QUOTE_NONE)
 for i in range (1000):
-    data = random.sample(range(-100, 100), 6)
+    data = numpy.random.uniform(low=-100.0, high=100.0, size=(6,))
+#    data = random.sample(range(-100.0, 100.0), 6)
     out.writerow(data)
 
 
